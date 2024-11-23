021f39d22985418c7e3d5a576af66cf37ff93383
sheldongomes <sheldongomesbr@gmail.com>
2024-11-22 19:27:16 -0500

    jknkj

--- audiohook/src/client/clientsession.ts
+++ audiohook/src/client/clientsession.ts
@@ -9,11 +9,11 @@ import {
 } from '../protocol/message';
 import {
     EventEntityDataTranscript
-} from '../protocol/entities-transcript'
+} from '../protocol/entities-transcript';
 import {
     Duration,
     LanguageCode,
-} from '../protocol/core'
+} from '../protocol/core';
 
 export type ClientSessionState = 
     | 'CONNECTING'
